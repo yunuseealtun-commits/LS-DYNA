d05c40aca8e78a6c1fe8a3d9f4b3ed7d5ab70dfb
yunuseealtun-commits <yunusee.altun@gmail.com>
2026-03-03 23:53:41 +0300

    Create get_dom.py

---
+++ Scripts/get_dom.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+opts = Options()
+opts.add_argument('--headless')
+driver = webdriver.Chrome(options=opts)
+
+try:
+    driver.get('http://localhost:8000/App/index.html')
+    time.sleep(3)
+    
+    root_inner = driver.execute_script("return document.getElementById('root').innerHTML")
+    print("ROOT INNER HTML LENGTH:", len(root_inner))
+    print("ROOT BROWSER PREVIEW:", root_inner[:500])
+except Exception as e:
+    print("ERROR:", e)
+finally:
+    driver.quit()
